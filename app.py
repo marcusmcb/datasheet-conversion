38683b91f7f2f9d52836c945846377262463221a
marcusmcb <marcusmcb@gmail.com>
2023-09-16 18:03:52 -0700

    updated python script

--- app.py
+++ app.py
@@ -10,21 +10,19 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 DB_NAME = 'coverland_data'
 COLLECTION_NAME = 'product_data'
 
-
 def insert_data_into_mongodb(data):
+    """Insert data into MongoDB."""
     client = MongoClient(MONGODB_URI)
     try:
         db = client[DB_NAME]
         collection = db[COLLECTION_NAME]
-
         # Insert each object in the array as separate documents in the collection
         collection.insert_many(data)
         print('Data inserted successfully!')
-    except Exception as e:
-        print('Error inserting data:', e)
+    except Exception as insert_error:  # Renamed from e to insert_error
+        print('Error inserting data:', insert_error)
     finally:
         client.close()
-
 
 if __name__ == "__main__":
     with open('./output.json', 'r') as file:
@@ -32,7 +30,7 @@ if __name__ == "__main__":
             data = json.load(file)
             insert_data_into_mongodb(data)
             print("ARRAY LENGTH:", len(data))
-        except json.JSONDecodeError as e:
-            print('Error parsing JSON:', e)
-        except Exception as e:
-            print('Error reading the file:', e)
+        except json.JSONDecodeError as json_error:  # Renamed for clarity
+            print('Error parsing JSON:', json_error)
+        except Exception as file_error:  # Renamed for clarity
+            print('Error reading the file:', file_error)
