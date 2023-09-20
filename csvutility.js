d62daee94cb77a9c10fdd8c850faf9cc729d0b2d
marcusmcb <marcusmcb@gmail.com>
2023-09-20 12:05:44 -0700

    added product sheet csv, updated csv utility to parse as JSON

--- csvutility.js
+++ csvutility.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
-const Papa = require('papaparse');
+const fs = require('fs')
+const Papa = require('papaparse')
 
-function csvToJson(filePath, outputFilePath) {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
+const csvToJson = (filePath, outputFilePath) => {
+	const fileContent = fs.readFileSync(filePath, 'utf8')
 
-    Papa.parse(fileContent, {
-        header: true,
-        complete: (result) => {
-            fs.writeFileSync(outputFilePath, JSON.stringify(result.data, null, 2));
-            console.log(`Data saved to ${outputFilePath}`);
-        }
-    });
+	Papa.parse(fileContent, {
+		header: true,
+		complete: (result) => {
+			fs.writeFileSync(outputFilePath, JSON.stringify(result.data, null, 2))
+			console.log(`Data saved to ${outputFilePath}`)
+		},
+	})
 }
 
-// Using the function to convert 'sample.csv' to 'mini_data_set.json'
-csvToJson('./data/updated_DB_test_sheet_mini.csv', 'mini_data_set.json');
+csvToJson('./data/updated_DB_test_sheet_mini.csv', 'mini_data_set.json')
+csvToJson('./data/coverland_data_20230828.csv', 'coverland_data_20230828.json')
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+const fs = require('fs')
 const dotenv = require('dotenv')
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb')
 
 dotenv.config()
 
@@ -8,33 +8,33 @@ const uri = process.env.MONGODB_URI
 const dbName = 'coverland_data_edited'
 const collectionName = 'product_lists'
 
-async function insertDataIntoMongoDB(data) {
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-    try {
-        await client.connect();
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
+const insertDataIntoMongoDB = (data) => {
+	const client = new MongoClient(uri, { useUnifiedTopology: true })
+	try {
+		await client.connect()
+		const db = client.db(dbName)
+		const collection = db.collection(collectionName)
 
-        // Insert each object in the array as a separate document in the collection
-        await collection.insertMany(data);
-        console.log('Data inserted successfully!');
-    } catch (err) {
-        console.error('Error inserting data:', err);
-    } finally {
-        await client.close();
-    }
+		// Insert each object in the array as a separate document in the collection
+		await collection.insertMany(data)
+		console.log('Data inserted successfully!')
+	} catch (err) {
+		console.error('Error inserting data:', err)
+	} finally {
+		await client.close()
+	}
 }
 
 fs.readFile('./mini_data_set.json', 'utf8', (err, jsonString) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
-    }
-    try {
-        const data = JSON.parse(jsonString);
-        insertDataIntoMongoDB(data);
-        console.log("ARRAY LENGTH: ", data.length)
-    } catch (error) {
-        console.error('Error parsing JSON:', error);
-    }
-});
+	if (err) {
+		console.error('Error reading the file:', err)
+		return
+	}
+	try {
+		const data = JSON.parse(jsonString)
+		insertDataIntoMongoDB(data)
+		console.log('ARRAY LENGTH: ', data.length)
+	} catch (error) {
+		console.error('Error parsing JSON:', error)
+	}
+})
