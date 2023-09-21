13c9122558ff9b2a49829019d6a01f9d662f671a
marcusmcb <marcusmcb@gmail.com>
2023-09-21 13:01:03 -0700

    code cleanup for product JSON data for eventual strapi collection

--- createStrapiCollections.js
+++ createStrapiCollections.js
@@ -1,8 +1,4 @@
 const products = require('./json/updated_DB_test_sheet_FULL.json')
-
-console.log('PRODUCT KEYS: ')
-console.log(Object.keys(products[0]))
-
 const uniqueMakes = {}
 const uniqueYears = {}
 
@@ -81,18 +77,19 @@ const extractUniqueSubmodels = (dataArray) => {
 	return uniqueSubmodels
 }
 
-const extractProducts = (data, modelNameToIdMap) => {  // add modelNameToIdMap as a parameter
+const extractProducts = (data, modelNameToIdMap) => {
+	// add modelNameToIdMap as a parameter
 	return data.map((item) => {
-		let submodelIDs = [];
-		if (item.sub1_id) submodelIDs.push(item.sub1_id);
-		if (item.sub2_id) submodelIDs.push(item.sub2_id);
-		if (item.sub3_id) submodelIDs.push(item.sub3_id);
+		let submodelIDs = []
+		if (item.sub1_id) submodelIDs.push(item.sub1_id)
+		if (item.sub2_id) submodelIDs.push(item.sub2_id)
+		if (item.sub3_id) submodelIDs.push(item.sub3_id)
 
 		return {
 			FK: item.FK,
 			type: item.type,
 			make_ID: item.make_ID,
-			model_id: modelNameToIdMap[item.model],  // lookup the model_id using the model name from the map
+			model_id: modelNameToIdMap[item.model], // lookup the model_id using the model name from the map
 			year_id: item.year_id,
 			submodel_ids: submodelIDs,
 			product_title: item.product_title,
@@ -100,18 +97,88 @@ const extractProducts = (data, modelNameToIdMap) => {  // add modelNameToIdMap a
 			price: item.price,
 			sale_price: item.sale_price,
 			htmlcontent: item.html_content,
-		};
-	});
+		}
+	})
 }
 
+const { modelsData, modelNameToIdMap } = generateModelsData(products) // destructuring
+const productArray = extractProducts(products, modelNameToIdMap)
 
-console.log(makesData)
-const { modelsData, modelNameToIdMap } = generateModelsData(products);  // destructuring
-console.log(modelsData);
+const parseProductImages = (productData) =>{
+	const imageTypes = [
+		{ prefix: 'uc_image', type: 'category' },
+		{ prefix: 'bannerimage', type: 'banner' },
+		{ prefix: 'bannerimage_m', type: 'banner_mobile' },
+		{ prefix: 'feature_', type: 'feature' },
+		{ prefix: 'product_', type: 'product' },
+	]
 
-console.log(generateYearData(products))
-console.log(extractUniqueSubmodels(products))
+	let productImages = []
+	let imageId = 0
 
-const productArray = extractProducts(products, modelNameToIdMap);
-console.log(productArray);
+	imageTypes.forEach((imgType) => {
+		const keys = Object.keys(productData).filter((key) =>
+			key.startsWith(imgType.prefix)
+		)
+
+		keys.forEach((key) => {
+			const urls = Array.isArray(productData[key])
+				? productData[key]
+				: productData[key].split(',')
+
+			urls.forEach((url) => {
+				if (url) {
+					const fileName = url.split('/').pop()
+
+					productImages.push({
+						image_id: imageId++,
+						FK: productData.FK,
+						image_type: imgType.type,
+						file_name: fileName,
+						image_link: url,
+					})
+				}
+			})
+		})
+	})
+
+	return productImages
+}
+
+const allProductImages = []
+
+products.forEach((product) => {
+	const productImages = parseProductImages(product)
+	allProductImages.push(...productImages)
+})
+
+const extractProductCharacteristics = (products) => {
+	return products.map((product) => {
+		return {
+			characteristic_id: product.FK, 
+			FK: product.FK,
+			weatherproof: product.weatherproof,
+			scratchproof: product.scratchproof,
+			uv_heat_protection: product.uv_heat_protection,
+			damp_protection: product.damp_protection,
+			hail_storm_and_snow_protection: product.hail_storm_and_snow_protection,
+			dust_and_abrasion_protection: product.dust_and_abrasion_protection,
+			mirror: product.mirror,
+			size: product.size,
+			characteristics_code: product.characteristics_code, 
+		}
+	})
+}
+
+const productCharacteristicsArray = extractProductCharacteristics(products)
+
+
+console.log(makesData[0])
+console.log(modelsData[0])
+console.log(generateYearData(products)[0])
+console.log(extractUniqueSubmodels(products)[0])
+console.log(productArray[0])
+console.log(allProductImages[0])
+console.log(productCharacteristicsArray[0])
+
 
