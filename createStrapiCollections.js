3bb464e3b9a200d2ad24328d7f934676df80c59c
marcusmcb <marcusmcb@gmail.com>
2023-09-21 12:22:17 -0700

    added logic to create product JSON data for strapi

--- createStrapiCollections.js
+++ createStrapiCollections.js
@@ -21,18 +21,21 @@ const generateModelsData = (products) => {
 	const uniqueModels = new Set()
 	let modelIdCounter = 1
 	const modelsData = []
+	const modelNameToIdMap = {} // map to store model name to its generated ID
 	products.forEach((product) => {
 		const model = product.model
 		if (!uniqueModels.has(model)) {
 			uniqueModels.add(model)
+			const model_id = `m${modelIdCounter}`
 			modelsData.push({
-				model_id: `m${modelIdCounter}`,
+				model_id,
 				model: model,
 			})
+			modelNameToIdMap[model] = model_id // save the model name and its ID to the map
 			modelIdCounter++
 		}
 	})
-	return modelsData
+	return { modelsData, modelNameToIdMap } // return both the modelsData and the map
 }
 
 const generateYearData = (data) => {
@@ -53,9 +56,8 @@ const generateYearData = (data) => {
 	return yearData
 }
 
-function extractUniqueSubmodels(dataArray) {
+const extractUniqueSubmodels = (dataArray) => {
 	const submodelsMap = {}
-
 	dataArray.forEach((item) => {
 		if (item.sub1_id && item.submodel1 && !submodelsMap[item.sub1_id]) {
 			submodelsMap[item.sub1_id] = item.submodel1
@@ -79,7 +81,37 @@ function extractUniqueSubmodels(dataArray) {
 	return uniqueSubmodels
 }
 
+const extractProducts = (data, modelNameToIdMap) => {  // add modelNameToIdMap as a parameter
+	return data.map((item) => {
+		let submodelIDs = [];
+		if (item.sub1_id) submodelIDs.push(item.sub1_id);
+		if (item.sub2_id) submodelIDs.push(item.sub2_id);
+		if (item.sub3_id) submodelIDs.push(item.sub3_id);
+
+		return {
+			FK: item.FK,
+			type: item.type,
+			make_ID: item.make_ID,
+			model_id: modelNameToIdMap[item.model],  // lookup the model_id using the model name from the map
+			year_id: item.year_id,
+			submodel_ids: submodelIDs,
+			product_title: item.product_title,
+			quantity: item.quantity,
+			price: item.price,
+			sale_price: item.sale_price,
+			htmlcontent: item.html_content,
+		};
+	});
+}
+
+
 console.log(makesData)
-console.log(generateModelsData(products))
+const { modelsData, modelNameToIdMap } = generateModelsData(products);  // destructuring
+console.log(modelsData);
+
 console.log(generateYearData(products))
 console.log(extractUniqueSubmodels(products))
+
+const productArray = extractProducts(products, modelNameToIdMap);
+console.log(productArray);
+
