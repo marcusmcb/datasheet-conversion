cc20f2e43d11a9228512de0c653954276803d7f7
marcusmcb <marcusmcb@gmail.com>
2023-12-15 06:49:24 -0800

    removed unused python script and utility files

--- csvutility.js
+++ csvutility.js
@@ -15,5 +15,6 @@ const csvToJson = (filePath, outputFilePath) => {
 
 // csvToJson('./data/NEW_master_product_sheet/master_product_sheet_112823.csv', './master_product_sheet_112823.json')
 // csvToJson('./data/master_product_sheet_120123.csv', './master_product_sheet_120123.json')
-csvToJson('./data/TO_TEST_master_product_sheet_121423.csv', './master_product_sheet_121423.json')
+// csvToJson('./data/TO_TEST_master_product_sheet_121423.csv', './master_product_sheet_121423.json')
+csvToJson('./data/review_sheet_test.csv', './review_sheet_test.json')
 // csvToJson('./data/default_image_update/default_model_image_set_TRUCK.csv', './json/default_model_image_set_TRUCK.json')
