36a7f696cdc4c501d986be48cf34b201b7f9d7a7
marcusmcb <marcusmcb@gmail.com>
2023-12-20 14:55:20 -0800

    added product review sheet

--- csvutility.js
+++ csvutility.js
@@ -16,5 +16,6 @@ const csvToJson = (filePath, outputFilePath) => {
 // csvToJson('./data/NEW_master_product_sheet/master_product_sheet_112823.csv', './master_product_sheet_112823.json')
 // csvToJson('./data/master_product_sheet_120123.csv', './master_product_sheet_120123.json')
 // csvToJson('./data/TO_TEST_master_product_sheet_121423.csv', './master_product_sheet_121423.json')
-csvToJson('./data/review_sheet_test.csv', './review_sheet_test.json')
+// csvToJson('./data/review_sheet_test.csv', './review_sheet_test.json')
+csvToJson('./data/review_sheet_122023.csv', './review_sheet_122023.json')
 // csvToJson('./data/default_image_update/default_model_image_set_TRUCK.csv', './json/default_model_image_set_TRUCK.json')
